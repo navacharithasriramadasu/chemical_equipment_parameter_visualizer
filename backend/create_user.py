5610c3f20fe303ae00cf2ba207ca62ebfdb1f54b
navacharithasriramadasu <navacharithasriramadasu@gmail.com>
2026-02-03 00:14:28 +0530

    second commit

--- backend/create_user.py
+++ backend/create_user.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 username = 'admin'
 password = 'admin'
-
 if not User.objects.filter(username=username).exists():
     user = User.objects.create_user(username=username, password=password)
     token = Token.objects.create(user=user)
